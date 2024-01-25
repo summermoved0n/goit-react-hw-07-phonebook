e6329d9765a987c10aab987a6ba77084f57a51f4
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-01-24 21:59:54 -0500

    test

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,4 @@
-import { createSelector } from '@reduxjs/toolkit';
+// import { createSelector } from '@reduxjs/toolkit';
 
 export const getContacts = state => {
   const { items } = state.phonebook.contacts;
